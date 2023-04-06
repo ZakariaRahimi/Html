c0b8542e1c6fe9753aae088e42a617ea8210cd40
Zakaria <72654177+ZakariaRahimi@users.noreply.github.com>
2023-04-06 11:33:14 +0430

    Reduce Function with different logics

--- main.js
+++ main.js
@@ -1,126 +1,145 @@
-const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const fin = arr.map(function (a) {
-    return {
-        value: a,
-        isEven: a % 2 === 0,
-        con: function (isEven) {
-            if (isEven) {
-                console.log("Even");
-            } else {
-                console.log("Odd");
-            }
-        }
-    }
+// const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const fin = arr.map(function (a) {
+//     return {
+//         value: a,
+//         isEven: a % 2 === 0,
+//         con: function (isEven) {
+//             if (isEven) {
+//                 console.log("Even");
+//             } else {
+//                 console.log("Odd");
+//             }
+//         }
+//     }
+// });
+
+//                                     // remained not successful
+// const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const dbl = arr.map(function (a) {
+//     return a;
+// });
+
+// const dbl2 = [];
+// for(arrayof in arr){
+//     dbl2.push(arrayof *2);
+// }
+
+// const dbl3 = arr.map(function (y){
+//     return y * 3;
+// }
+// );
+
+//  value = ()=>{
+//     return 'something' 
+// }
+
+// const tryit = ()=> {
+//     return "Try Again";
+// }
+
+// const come = (x,y)=>{
+//     if (x >= y){
+//         return `x is Greater`
+//     }else{
+//         return 'y is greater'
+//     }
+// }
+
+// const square = x =>{
+//     return x;
+// }
+
+// const square = (n)=> n*n;   // Implicit Return
+
+
+// const triple = (n) =>(       // Implicit Return (We Only Can to return one thing like one expression  )
+//     n*n*n
+// )
+
+// const come = (x,y)=>(
+//     if (x >= y){
+//         return `x is Greater`      // this code is Error because it has more returns not only one expression
+//     }else{
+//         return 'y is greater'
+//     }
+// )
+
+// const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const triple = arr.map(n => n*3)
+
+
+// const arr = ['try','wish','get','wind'];
+// const getit = arr.find(f =>{
+//     return f.includes('wi');
+// })
+
+// const num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const word = ['some','again','try','going','sort','trust','gorgeous','agreculture','thing'];
+// const letnum = num.filter(n =>(n%2==0))      // this code is working
+
+// const letword = word.filter(w=>(w.toString().concat('something')));  // this code is outputing unexpected result
+
+// const arr = ['the main point','world of the game','live of the people'];
+// const result = arr.filter(r =>{
+//     return r.includes('the');
+// })
+
+
+// const arr = ['fruits','apples','beries'];
+// const hasLenth = arr.every(e => e.length===6);
+
+// const hasS = arr.every(l=>{
+//     const last = l.length -1;
+//     return l[last] === 's'
+// });
+
+// const arr = ['fruits','peach','beries'];
+// const hasLenth = arr.some(e => e.length===6);
+
+// const hasS = arr.some(l=>{
+//     const last = l.length -1;
+//     return l[last] === 's'
+// });
+
+// const arr = ['fruits','peach','beries'];
+
+// const sort1 = arr.sort();
+
+// const sort2 = arr.slice().sort();
+
+// const num = [0,61,27,433,49,95,69,97,98,99,340,200,400,3000];
+
+// const numSort = num.sort();
+
+// const numSortAsc = num.sort((a, b)=> a - b);
+
+// const numSortDesc = num.sort((a, b)=> b - a);
+
+// const numSortSlice = num.slice().sort();
+
+// const numSortAscSlice = num.slice().sort((a, b)=> a - b);
+
+// const numSortDescSlice = num.slice().sort((a, b)=> b - a);
+
+const num = [0,6,7,3,9,5,6,7,9,9,4,2,40,30];
+
+const maxNum = num.reduce((max, current)=>{
+    if(current >max) return current;             // Maximum Value
+    return max;
 });
 
-                                    // remained not successful
-const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const dbl = arr.map(function (a) {
-    return a;
+const minNum = num.reduce((min, current)=>{
+    if(current < min) return current;             // Minimum Value
+    return min;
 });
 
-const dbl2 = [];
-for(arrayof in arr){
-    dbl2.push(arrayof *2);
-}
 
-const dbl3 = arr.map(function (y){
-    return y * 3;
-}
-);
-
- value = ()=>{
-    return 'something' 
-}
-
-const tryit = ()=> {
-    return "Try Again";
-}
-
-const come = (x,y)=>{
-    if (x >= y){
-        return `x is Greater`
-    }else{
-        return 'y is greater'
-    }
-}
-
-const square = x =>{
-    return x;
-}
-
-const square = (n)=> n*n;   // Implicit Return
-
-
-const triple = (n) =>(       // Implicit Return (We Only Can to return one thing like one expression  )
-    n*n*n
-)
-
-const come = (x,y)=>(
-    if (x >= y){
-        return `x is Greater`      // this code is Error because it has more returns not only one expression
-    }else{
-        return 'y is greater'
-    }
-)
-
-const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const triple = arr.map(n => n*3)
-
-
-const arr = ['try','wish','get','wind'];
-const getit = arr.find(f =>{
-    return f.includes('wi');
+const maximum = num.reduce((max, cur)=>{
+    return Math.max(max, cur);                   // Maximum Value bye Buit-in Method
 })
 
-const num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const word = ['some','again','try','going','sort','trust','gorgeous','agreculture','thing'];
-const letnum = num.filter(n =>(n%2==0))      // this code is working
-
-const letword = word.filter(w=>(w.toString().concat('something')));  // this code is outputing unexpected result
-
-const arr = ['the main point','world of the game','live of the people'];
-const result = arr.filter(r =>{
-    return r.includes('the');
+const minimum = num.reduce((min, cur)=>{
+    return Math.min(min, cur);                  // Minimun Value bye Built-in Mothod
 })
-
-
-const arr = ['fruits','apples','beries'];
-const hasLenth = arr.every(e => e.length===6);
-
-const hasS = arr.every(l=>{
-    const last = l.length -1;
-    return l[last] === 's'
-});
-
-const arr = ['fruits','peach','beries'];
-const hasLenth = arr.some(e => e.length===6);
-
-const hasS = arr.some(l=>{
-    const last = l.length -1;
-    return l[last] === 's'
-});
-
-const arr = ['fruits','peach','beries'];
-
-const sort1 = arr.sort();
-
-const sort2 = arr.slice().sort();
-
-const num = [0,61,27,433,49,95,69,97,98,99,340,200,400,3000];
-
-const numSort = num.sort();
-
-const numSortAsc = num.sort((a, b)=> a - b);
-
-const numSortDesc = num.sort((a, b)=> b - a);
-
-const numSortSlice = num.slice().sort();
-
-const numSortAscSlice = num.slice().sort((a, b)=> a - b);
-
-const numSortDescSlice = num.slice().sort((a, b)=> b - a);
-
-
 
 
